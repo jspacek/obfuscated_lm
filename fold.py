38245e856eb105fe69ac464e84d280366ad1d0d8
jspacek <jodi.spacek@gmail.com>
2017-03-19 11:29:24 -0700

    Fix folding concatenation, print out ngram counts, perplexity, and word probabilities

--- fold.py
+++ fold.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import os, sys
+import os, sys, fileinput
 
 def fold():
     from sklearn.model_selection import KFold
@@ -10,35 +10,38 @@ def fold():
             fvalidation.append(file)
 
     print fvalidation
-    # Create the ngram counts and ngram files for trigram of all the token files
-    for file in fvalidation:
-        #./ngram-count -text burton/prog.c -order 2 -addsmooth 1 -write burton.count -lm burton.lm
-        os.system('./bin/ngram-count -text output/%s -addsmooth 1 -lm train/%s.lm -order 3 -interpolate' % (file, file))
-        #os.system('./bin/ngram-count -text output/%s -lm train/%s.lm -order 3 -unk -kndiscount -wbdiscount3 -interpolate' % (file, file))
-        os.system('./bin/ngram -lm train/%s.lm -order 3 -ppl -write train/%s.3grams.count' % (file, file))
-        # ./ngram -lm corpus.lm -ppl test_corpus2.txt -debug 2
 
-    kf = KFold(n_splits=10)
+    # Create a 10 fold cross validation with randomized order
+    kf = KFold(n_splits=10, shuffle=True)
     foldcount = 0
     for train, test in kf.split(fvalidation):
         print("%s %s" % (train, test))
         trainlist = train.tolist()
         testlist = test.tolist()
-        with open("train/fold%d.test" % foldcount, 'wb') as outtest:
-            for testindex in testlist:
-                testfile = fvalidation[testindex]
-                #print("TEST %d %s" % (testindex, testfile))
-                outtest.write(testfile + '\n')
-
+        # Put all of the training file tokens into the current fold training
         with open("train/fold%d.train" % foldcount, 'wb') as outtrain:
             for trainindex in trainlist:
-                trainfile = fvalidation[trainindex]
-                #print("TRAIN %d %s" % (trainindex, trainfile))
-                outtrain.write(trainfile + '\n')
+                ftrain = 'output/%s' % fvalidation[trainindex]
+                fin = fileinput.input(ftrain)
+                for line in fin:
+                    outtrain.write(line)
+                fin.close()
+
+        # Generate a language model for this fold's concatenated training file
+        os.system('./bin/ngram-count -text train/fold%d.train -addsmooth 1 -lm train/fold%d.train.kn.lm.gz -order 3 -interpolate'
+        % (foldcount, foldcount))
+
+        with open("train/fold%d.test" % foldcount, 'wb') as outtest:
+            for testindex in testlist:
+                ftest = fvalidation[testindex]
+                print("TEST %d %s" % (testindex, ftest))
+                # Record the tested files in this fold
+                outtest.write(ftest + '\n')
+                # test the file on trigrams with debug set to word level and record perplexity
+                os.system('./bin/ngram -lm train/fold%d.train.kn.lm.gz -order 3 -ppl output/%s -write-lm train/fold%d.%s.3grams -debug 2 > train/fold%d.%s.3grams.count'
+                % (foldcount, ftest, foldcount, ftest, foldcount, ftest))
 
         foldcount += 1
-
-        # TODO believe we can get rid of the lm files at this point and just leave the 3grams count files
 
 if __name__ == '__main__':
     fold()
